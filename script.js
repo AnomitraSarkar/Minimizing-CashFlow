aaca4ac982fdff2a68c9c68177523d0de74f7fcf
Anomitra Sarkar <81612233+AnomitraSarkar@users.noreply.github.com>
2023-11-06 23:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("DOM JS running live")
+
+function addToNodeManage(nodename){
+    console.log("Added to Node List: ",nodename)
+}
+
+function addToEdgeManage(start,end,amt){
+    console.log("Added to Edge List: ",start,end,amt)
+    
+}
